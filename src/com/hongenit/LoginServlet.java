d169e3bf8f54c38b35d43b0bddc6c853e991ae27
xiaohongrc <chenxiaohong@rcplatformhk.com>
2019-06-25 18:30:33 +0800

    Initial commit

---
+++ src/com/hongenit/LoginServlet.java
@@ -0,0 +1,21 @@
+package com.hongenit;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/checkLogin")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("dopost");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doget1");
+        response.getWriter().print("what's wrong");
+
+    }
+}
