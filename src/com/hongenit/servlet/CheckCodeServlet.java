6d1f3f4c6b49111b88571dcdf766f376ba7f2efc
xiaohongrc <chenxiaohong@rcplatformhk.com>
2019-06-28 19:11:06 +0800

    add login servlet

--- src/com/hongenit/servlet/CheckCodeServlet.java
+++ src/com/hongenit/servlet/CheckCodeServlet.java
@@ -1,25 +1,26 @@
-package com.hongenit;
+package com.hongenit.servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/checkLogin")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/checkCode")
+public class CheckCodeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("dopost");
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("LoginServlet doget1");
-        String code = (String) request.getSession().getAttribute("checkCode");
-        System.out.println("LoginServlet  code =  " + code);
 
-
-        response.getWriter().print("what's wrong");
-
+        String code = "1243";
+        HttpSession session = request.getSession();
+        String checkCode = (String) session.getAttribute("checkCode");
+        System.out.println("CheckCodeServlet code  = " + checkCode);
+        session.setAttribute("checkCode", code);
+        response.getWriter().print(code);
     }
 }
