05f79a829472ddc54bbeca9a96a3f246c77dcb1b
hongenit <hongenit@126.com>
2019-06-30 23:04:05 +0800

    add SignUpServlet

---
+++ src/com/hongenit/servlet/SignUpServlet.java
@@ -0,0 +1,22 @@
+package com.hongenit.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("user/sign_up")
+public class SignUpServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        response.getWriter().println(email+"succeed");
+
+
+    }
+}
